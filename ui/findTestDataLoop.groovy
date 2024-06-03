001b6a307490d3c81e02e3af9ef9c86839514b33
alston argodi <19081010163@student.upnjatim.ac.id>
2024-06-03 18:03:16 +0700

    add: find test loop

---
+++ ui/findTestDataLoop.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+//addon
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.WebDriver as WebDriver
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.By as By
+
+def searchData = findTestData(//masukin path test data disini bentuk csv / excel)
+Int dataRow = searchData.getRowNumbers() // mengambil jumlah row dalam tabel
+Int dataColumn = searchData.getColumnNumbers() // mengambil jumlah kolom dalam tabel
+
+for(int i=1;i < dataRow.size();i++) { //disini melakukan perulangan untuk mengambil data pada setiap row dalam tabel 
+    for(int j=1;j < dataColumn.size();j++){ //perulangan untuk mengambil data pada kolom dalam tabel
+
+        //ambil data dari tabel tadi berdasarkan index .getValue(index kolom,index row)
+        searchValue = searchData.getValue(j,i)
+    }
+}
